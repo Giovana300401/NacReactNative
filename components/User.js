634fbe2419d146685e2f07de097d3fd105fa8e09
Giovana Lira Fernandes <48058149+Giovana300401@users.noreply.github.com>
2020-05-17 20:24:43 -0300

    Terminando User component

--- components/User.js
+++ components/User.js
@@ -6,6 +6,9 @@ export default class User extends Component{
       super(props);
 
       this.state = {
+        id : 0,
+        email: '',
+        imagem: '',
         first_name: '',
         last_name: '',
       };
@@ -21,11 +24,23 @@ export default class User extends Component{
     .then(response => response.json())
     .then((responseJson)=> {
 
+      var id = responseJson['data']['id']
+   
+      var email = responseJson['data']['email']
+
+      var imagem = responseJson['data']['avatar']
+
       var primeiroNome = responseJson['data']['first_name']
+
       var ultimoNome = responseJson['data']['last_name']
 
-      this.setState({first_name: primeiroNome,
-                  last_name: ultimoNome,})
+      this.setState({
+        
+        id: id,
+        email: email,
+        imagem: imagem,
+        first_name: primeiroNome,
+        last_name: ultimoNome,})
 
     })
     .catch(error=>console.log(error))
@@ -34,10 +49,10 @@ export default class User extends Component{
   render(){
     return(
       <View>
-        <Text>Username: </Text>
-        <Image>User Image: </Image>
-        <Text>User Id: </Text>
-        <Text>User E-mail: </Text>
+        <Text>{this.state.nome} </Text>
+        <Image source={{url:this.state.imagem}}></Image>
+        <Text>{this.state.id} </Text>
+        <Text>{this.state.email} </Text>
       </View>
     );
   }
